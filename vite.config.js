2f9ac394caf9604979f4158a3738799e7399be43
Issam Aloui <mrdivanodz@gmail.com>
2025-06-22 15:49:37 +0100

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Use relative paths for assets
+  base: "/portfolio/",  
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    sourcemap: false, // Disable sourcemaps in production
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
